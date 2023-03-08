0aa675d8588a47e830caa4f0b80083a11ec34315
Felix Philips <www.felix4real98@gmail.com>
2023-03-08 21:31:40 +0100

    changed client server host

--- client/src/constants/endpoints.js
+++ client/src/constants/endpoints.js
@@ -1,5 +1,4 @@
-// const host = "https://sms-automation-kp.herokuapp.com"
-const host = "http://localhost:5000";
+const host = "sms-automation.up.railway.app";
 export const loginURL = `${host}/auth/login`;
 export const registerURL = `${host}/auth/register`;
 export const allMessages = `${host}/sms/messages`;
